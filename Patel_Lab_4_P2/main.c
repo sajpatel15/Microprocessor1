ba2f1b62e3f8cd719c42c5ecba321f4b9f35b72a
sajpatel15 <65937250+sajpatel15@users.noreply.github.com>
2021-03-02 20:29:31 -0700

    Add files via upload

---
+++ Patel_Lab_4_P2/main.c
@@ -0,0 +1,141 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "inc/hw_gpio.h" // Critical Function GPIO Protection
+#include "driverlib/interrupt.h"
+
+#define RED GPIO_PIN_1
+#define GREEN GPIO_PIN_3
+#define BLUE GPIO_PIN_2
+
+#define BUTTON_1 GPIO_PIN_4
+
+volatile char isr_flag_1 = 0;
+int i;
+
+void GPIO_IRQHandler(void)
+{
+
+    if (GPIOIntStatus(GPIO_PORTF_BASE, 1) == 16)
+    {
+        // Clear Flag
+        GPIOIntClear(GPIO_PORTF_BASE, BUTTON_1);
+        for (i = 0; i < 5000; i++) //Blank for loop for delay
+        {
+
+        }
+        /* Handler Code Below */
+        if (GPIOPinRead(GPIO_PORTF_BASE, BUTTON_1) != 0) //Check if the button has been released to update the flag
+            isr_flag_1 = 1;
+    }
+
+}
+
+void configure_LEDs(void)
+{
+    //Enable Port F as GPIO
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    //Set PF(2,2,3) to outputs. On Board LEDs
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, RED | GREEN | BLUE);
+    // Turn Off LEDs
+    GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0x00);
+
+    // Initialize Button 1
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, BUTTON_1);
+    GPIOPadConfigSet(GPIO_PORTF_BASE, BUTTON_1, GPIO_STRENGTH_2MA,
+    GPIO_PIN_TYPE_STD_WPU);
+
+}
+
+void configure_PF_Int()
+{
+    // Button 1
+    GPIOIntTypeSet(GPIO_PORTF_BASE, BUTTON_1, GPIO_RISING_EDGE);
+
+    GPIOIntRegister(GPIO_PORTF_BASE, GPIO_IRQHandler);
+    GPIOIntEnable(GPIO_PORTF_BASE, BUTTON_1);
+
+    IntEnable(INT_GPIOF);
+
+    IntMasterEnable();
+}
+
+int main(void)
+{
+    //Set System clock to 40 MHz
+    SysCtlClockSet(
+    SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+
+    /* Configure LEDs */
+    configure_LEDs();
+
+    /* Configure Button1 as Interrupt */
+    configure_PF_Int();
+
+    while (1)
+    {
+        // PA4 - External Button Pressed
+        if (isr_flag_1)
+        {
+
+            isr_flag_1 = 0;
+
+            // turning on the R LED  only
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0b00000010);
+
+            // turns the R LED off
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0b00000000);
+
+            // turns the RG LEDs on
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0b00000110);
+
+            // turns the RG LEDs off
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0b00000000);
+
+            // turns the RGB LEDs on
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0b00001110);
+
+            // turns the RGB LEDs off
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0b00000000);
+
+            // turns the GB LEDs on
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0b00001100);
+
+            // turns the GB LEDs off
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0b00000000);
+
+            // turns the B LEDs on
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0b00000100);
+
+            // turns the B LEDs off
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0b00000000);
+
+            // turns the RB LEDs on
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0b00000110);
+
+            // turns the RB LEDs off
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0b00000000);
+
+        }
+
+    }
+
+}
+
---
+++ Patel_Lab_4_P3/main.c
@@ -0,0 +1,178 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "inc/hw_gpio.h" // Critical Function GPIO Protection
+#include "driverlib/interrupt.h"
+
+#define RED GPIO_PIN_1
+#define GREEN GPIO_PIN_3
+#define BLUE GPIO_PIN_2
+
+#define BUTTON_1 GPIO_PIN_4
+#define BUTTON_2 GPIO_PIN_0
+
+volatile char isr_flag_1 = 0;
+volatile char isr_flag_2 = 0;
+
+int oper_1[10] = { 1, 2, 3, 4, 1, 2, 3, 4, 1, 2 };
+int oper_2[10] = { -2, 2, 3, 4, 1, 2, 3, 4, 1, 2 };
+int result[10];
+
+int i;
+int j = 0;
+int count = 0;
+
+void GPIO_IRQHandler(void)
+{
+
+    if (GPIOIntStatus(GPIO_PORTF_BASE, 1) == 16)
+    {
+        // Clear Flag
+        GPIOIntClear(GPIO_PORTF_BASE, BUTTON_1);
+        for (i = 0; i < 5000; i++) //Blank for loop for delay
+        {
+
+        }
+        /* Handler Code Below */
+        if (GPIOPinRead(GPIO_PORTF_BASE, BUTTON_1) != 0) //Check if the button has been released to update the flag
+            isr_flag_1 = 1;
+    }
+
+    if (GPIOIntStatus(GPIO_PORTF_BASE, 1) == 1)
+    {
+        // Clear Flag
+        GPIOIntClear(GPIO_PORTF_BASE, BUTTON_2);
+        for (i = 0; i < 5000; i++) //Blank for loop for delay
+        {
+
+        }
+        /* Handler Code Below */
+        if (GPIOPinRead(GPIO_PORTF_BASE, BUTTON_2) != 0) //Check if the button has been released to update the flag
+            isr_flag_2 = 1;
+    }
+
+}
+
+void configure_LEDs(void)
+{
+    //Enable Port F as GPIO
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    //Set PF(2,2,3) to outputs. On Board LEDs
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, RED | GREEN | BLUE);
+    // Turn Off LEDs
+    GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0x00);
+
+    // Initialize Button 1
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, BUTTON_1);
+    GPIOPadConfigSet(GPIO_PORTF_BASE, BUTTON_1, GPIO_STRENGTH_2MA,
+    GPIO_PIN_TYPE_STD_WPU);
+
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = 0x4C4F434B;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_CR) |= 0x01;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = 0;
+
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, BUTTON_2);
+    GPIOPadConfigSet(GPIO_PORTF_BASE, BUTTON_2, GPIO_STRENGTH_2MA,
+    GPIO_PIN_TYPE_STD_WPU);
+
+}
+
+void configure_PF_Int()
+{
+    // Button 1
+    GPIOIntTypeSet(GPIO_PORTF_BASE, BUTTON_1, GPIO_RISING_EDGE);
+    GPIOIntTypeSet(GPIO_PORTF_BASE, BUTTON_2, GPIO_RISING_EDGE);
+
+    GPIOIntRegister(GPIO_PORTF_BASE, GPIO_IRQHandler);
+    GPIOIntEnable(GPIO_PORTF_BASE, BUTTON_1);
+    GPIOIntEnable(GPIO_PORTF_BASE, BUTTON_2);
+
+    IntEnable(INT_GPIOF);
+
+    IntMasterEnable();
+}
+
+int main(void)
+{
+    //Set System clock to 40 MHz
+    SysCtlClockSet(
+    SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+
+    /* Configure LEDs */
+    configure_LEDs();
+
+    /* Configure Button1 as Interrupt */
+    configure_PF_Int();
+
+    while (1)
+    {
+        // PA4 - External Button Pressed
+        if (isr_flag_1)
+        {
+            count++;
+
+            if (count > 3)
+            {
+                count = 1;
+            }
+            isr_flag_1 = 0;
+            if (count == 1)
+            {
+                GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0b00000000);
+                GPIOPinWrite(GPIO_PORTF_BASE, RED, 0b00000010);
+            }
+            else if (count == 2)
+            {
+                GPIOPinWrite(GPIO_PORTF_BASE, GREEN | RED | BLUE, 0b00000000);
+                GPIOPinWrite(GPIO_PORTF_BASE, GREEN, 0b00001000);
+            }
+            else if (count == 3)
+            {
+                GPIOPinWrite(GPIO_PORTF_BASE, GREEN | RED | BLUE, 0b00000000);
+                GPIOPinWrite(GPIO_PORTF_BASE, BLUE, 0b00000100);
+            }
+        }
+        else if (isr_flag_2)
+        {
+
+            isr_flag_2 = 0;
+
+
+            if (count == 1)
+            {
+                result[j] = oper_1[j] + oper_2[j];
+                GPIOPinWrite(GPIO_PORTF_BASE, RED, 0x00);
+                j++;
+            }
+            else if (count == 2)
+            {
+                result[j] = oper_1[j] - oper_2[j];
+                GPIOPinWrite(GPIO_PORTF_BASE, GREEN, 0x00);
+                j++;
+            }
+            else if (count == 3)
+            {
+                result[j] = oper_1[j] * oper_2[j];
+                GPIOPinWrite(GPIO_PORTF_BASE, BLUE, 0x00);
+                j++;
+            }
+
+            if (j == 10)
+            {
+                GPIOPinWrite(GPIO_PORTF_BASE, RED | GREEN | BLUE, 0b00001110);
+                break; // exit while(1)
+            }
+
+            count = 0;
+
+        }
+
+    }
+
+}
+
